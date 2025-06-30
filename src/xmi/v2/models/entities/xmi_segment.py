e18342613bbfd93bc1be2f55ac717dce08c294a7
Freddie Kuo <freddie.c.kuo@gmail.com>
2025-06-30 17:31:55 +0800

    XMI-175: Python Xmi Library Unit Testing
    Updates to xmi_segment, xmi_structural_cross_section

--- src/xmi/v2/models/entities/xmi_segment.py
+++ src/xmi/v2/models/entities/xmi_segment.py
@@ -1,34 +1,13 @@
 from pydantic import Field, field_validator, model_validator
 from ..bases.xmi_base_entity import XmiBaseEntity
-from ..bases.xmi_base_geometry import XmiBaseGeometry
-from .xmi_structural_point_connection import XmiStructuralPointConnection
-from .xmi_structural_storey import XmiStructuralStorey
 from ..enums.xmi_segment_type_enum import XmiSegmentTypeEnum
-from ..geometries.xmi_point_3d import XmiPoint3D
 
 class XmiSegment(XmiBaseEntity):
-    geometry: XmiBaseGeometry = Field(..., alias="Geometry")
     position: int = Field(..., alias="Position")
-    begin_node: XmiStructuralPointConnection = Field(..., alias="BeginNode")
-    end_node: XmiStructuralPointConnection = Field(..., alias="EndNode")
     segment_type: XmiSegmentTypeEnum = Field(..., alias="SegmentType")
 
     class Config:
         populate_by_name = True
-
-    @field_validator("geometry")
-    @classmethod
-    def validate_geometry(cls, v):
-        if not isinstance(v, XmiBaseGeometry):
-            raise TypeError("geometry should be of type XmiBaseGeometry")
-        return v
-
-    @field_validator("begin_node", "end_node")
-    @classmethod
-    def validate_node(cls, v):
-        if not isinstance(v, XmiStructuralPointConnection):
-            raise TypeError("Nodes should be of type XmiStructuralPointConnection")
-        return v
 
     @field_validator("segment_type")
     @classmethod
---
+++ tests/xmi/v2/test_entities/test_xmi_structural_cross_section.py
@@ -0,0 +1,15 @@
+from src.xmi.v2.models.entities.xmi_structural_cross_section import XmiStructuralCrossSection
+from src.xmi.v2.models.enums.xmi_shape_enum import XmiShapeEnum
+from tests.xmi.v2.test_inputs.xmi_structural_cross_section_input import valid_cross_section_input
+
+def test_cross_section_without_material():
+    instance, errors = XmiStructuralCrossSection.from_dict(valid_cross_section_input)
+
+    assert not errors, f"Unexpected validation errors: {errors}"
+    assert instance is not None
+    assert instance.name == "300 x 300mm"
+    assert instance.shape == XmiShapeEnum.UNKNOWN
+    assert instance.parameters == (300.0, 300.0)
+
+
+# .venv/bin/python -m pytest tests/xmi/v2/test_entities/test_xmi_structural_cross_section.py
